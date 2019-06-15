8085b9b0d99f65172510c63a1bafb359eb0d601f
James Thistlewood <jamesthistlewood@gmail.com>
2019-06-15 19:55:14 +0100

    add proper daemon script

---
+++ daemon.sh
@@ -0,0 +1,60 @@
+#!/usr/bin/env bash
+
+### BEGIN INIT INFO
+# Provides:		    musebot
+# Required-Start:	$remote_fs $syslog
+# Required-Stop:	$remote_fs $syslog
+# Default-Start:	2 3 4 5
+# Default-Stop:		0 1 6
+# Short-Description:	Runs MuseBot
+### END INIT INFO
+
+# This script should be copied to /etc/init.d
+
+SRCLOCATION=/home/pi/MuseBot        # Change this to the absolute location of the musebot installation
+PID_FILE=/tmp/musebot.pids
+
+start() {
+	echo "Starting..."
+	if [ -f $PID_FILE ]; then
+		echo "Service already started!"
+	else
+		"$SRCLOCATION"/main.py &
+        pid=$!
+		echo $pid >> $PID_FILE
+
+        wait $pid
+        
+        # restart on process exit
+        stop
+        echo "Restarting..."
+        start
+	fi
+}
+
+stop() {
+	echo "Stopping..."
+	kill $(cat $PID_FILE)
+	rm $PID_FILE
+	echo "Stopped."
+}
+
+case "$1" in
+    start)
+        start
+        ;;
+    stop)
+        stop
+        ;;
+    restart)
+        stop
+        start
+        ;;
+    status)
+        ;;
+    *)
+	echo "Usage: $0 {start|stop|status|restart}"
+	;;
+esac
+
+exit 0
