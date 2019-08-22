39c33ce5879b84eabc306e2915dc54275ac425f9
James Thistlewood <jamesthistlewood@gmail.com>
2019-08-22 14:56:57 +0100

    update hook base dir handling

--- hooks/config.php
+++ hooks/config.php
@@ -3,10 +3,9 @@
 	ini_set('display_startup_errors', 1);
 	error_reporting(E_ALL);
 
+	// This must be created and chown'd to the user that handles php requests.
+	// This might be www-data, for example, or apache.
     define("BASE_DIR", "/home/james/MuseBot/queue/");
-
-	if (!file_exists(BASE_DIR))
-        mkdir(BASE_DIR, 0744);
 
     include_once("./secrets.php");
 ?>
