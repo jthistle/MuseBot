51c93a0ac7121fa11be24e943d200785f3013f9b
James Thistlewood <jamesthistlewood@gmail.com>
2019-08-28 09:56:01 +0100

    improve handling of webhook config

--- hooks/config.php
+++ hooks/config.php
@@ -3,9 +3,11 @@
 	ini_set('display_startup_errors', 1);
 	error_reporting(E_ALL);
 
+    include_once("./secrets.php");
+
 	// This must be created and chown'd to the user that handles php requests.
 	// This might be www-data, for example, or apache.
-    define("BASE_DIR", "/home/james/MuseBot/queue/");
+	if (BASE_DIR == null)
+		define("BASE_DIR", "/home/james/MuseBot/queue/");
 
-    include_once("./secrets.php");
 ?>
